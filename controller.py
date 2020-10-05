c2b379d96dadd10256893e49ec63833f80f96f05
Nidhish Gandhi <gnidhishji3256@gmail.com>
2020-10-05 12:02:09 +0530

    fourth

--- controller.py
+++ controller.py
@@ -9,7 +9,18 @@ class Controller:
         # Connect signals and slots
         self._connectSignals()
 
-   
+    def _calculateResult(self):
+        """Evaluate expressions."""
+        result = self._evaluate(expression=self._view.getDisplayText())
+        self._view.setDisplayText(result)
+
+    def _buildExpression(self, sub_exp):
+        """Build expression."""
+        if self._view.getDisplayText() == ERROR_MSG:
+            self._view.clearDisplay()
+
+        expression = self._view.getDisplayText() + sub_exp
+        self._view.setDisplayText(expression)
     def _connectSignals(self):
         """Connect signals and slots."""
         for btnText, btn in self._view.buttons.items():
